ba39d9014522e71206988911d55884dc03b71a73
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-02-17 13:45:38 +0200

    Create routes

---
+++ folio/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {Switch} from 'react-router-dom';
+import {Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+            <Route>
+
+            </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
