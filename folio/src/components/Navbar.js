9a4e2e8d7bcb6d43c9ed7a6b19a7ec701eebddd6
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-02-26 22:24:09 +0200

    navbar positioning and clean up

--- folio/src/components/Navbar.js
+++ folio/src/components/Navbar.js
@@ -1,47 +1,50 @@
+import {Link} from 'react-router-dom';
+
 const Navbar= ()=>{
 
     return(
 
-       
-
-
 <>
 
-<nav className="navbar navbar-expand-lg bg-body-tertiary sticky-top" id="Navbar">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">STUDIONTI</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse " id="navbarSupportedContent">
-      <div>
-        
+
+  <nav class="navbar fixed-top ">
+    
+      <div className="d-flex">
+        <span>
+          <Link to="/">STUDIO ONTI</Link>
+        </span>
       </div>
-        <ul className="navbar-nav  ">
-          <li className="nav-item ">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Work</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">My process</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
-          </li>
-        
-        </ul>
-      
-    </div>
-  </div>
-</nav>
+      <div className="d-none d-lg-flex gap-5  me-2 ">
+          <span> <Link to="/about">About </Link></span>
+          <span> <Link to="/work">Work </Link></span>
+          <span> <Link to="/process">My process </Link></span>
+          <span> <Link to="/contact">Contact </Link></span>
+      </div>
+      <div className="d-lg-none">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+
+      <div class="offcanvas w-75  offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+            <Link to="/">
+              STUDIO ONTI
+            </Link>
+          </h5>
+        </div>
+          <hr/>
+          <Link className=" mx-auto" to="/about"  >About</Link>
+          <hr />
+          <Link className="mx-auto" to="/work">Work</Link>
+          <hr/>
+          <Link className="mx-auto" to="/process">My process</Link>
+          <hr/>
+          <Link className="mx-auto" to="/contact">Contact</Link>
+      </div>
+  </nav>
+ 
 </>
     );
 };
