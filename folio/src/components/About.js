454954b4cd53165a04c79c2cdca5317e980edaba
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-02-26 22:23:15 +0200

    code clean up and arrange elements

--- folio/src/components/About.js
+++ folio/src/components/About.js
@@ -1,46 +1,46 @@
-import Hero from './Hero';
 import gsap from 'gsap';
 import {useEffect} from  'react';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const About=()=>{
 
-    {/*gsap animation*/}
+
+ const tl=gsap.timeline();
+
     useEffect(()=>{
-       gsap.fromTo("#about-me-video",
+        
+        tl.fromTo("#about-me-video",
         {x:"-100vw"},
         { 
             x:"0vw",
             scrollTrigger:{
             trigger:"#biography",
-            start:"bottom bottom",
-            end:"top bottom",
-            markers:true,
-            scrub:2,
-
-        }}) 
-    },[])
-
-
-
-    
-    return(
-        <>
+            start:"top 99.8%",
+            end:"top bottom",   
+            toggleAction:"play pause none none",
+            
+        }})
         
-            <div className="min-vh-100 container-fluid" id="about-container">
+    })
+
+    return(
+        <> 
+            <div className="min-vh-100 container-fluid position-relative" id="about-container">
+                
                 <div className="row row-cols-1 row-cols-lg-2">
-                        <div className="col border" id="about-container">
-                        </div>
-                        {/* below video and animation*/}
-                         <div className="col-lg-12 bg-warning position-relative" id="biography">
-                                <p>biography</p>
+                         <div className="min-vh-100 col-lg-12 bg-warning position-relative  " id="about-intro">
+                                <p>intro</p>
                                 <div className="col-lg-12 bg-success position-absolute top-0 start-0 bottom-0 end-0" id="about-me-video">
                                     Video
                                 </div>
                             </div>
+                         <div  className="min-vh-100 col-lg-12 border" id="biography">
+                                <p>Biography</p>
+                        </div>    
                          <div  className="min-vh-100 col-lg-12 border" id="statement">
                                 <p>Statement</p>
-                            </div>
+                        </div>
                 </div>
             </div>
         </>
