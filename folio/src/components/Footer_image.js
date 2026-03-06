dcc764cb10c756a669a50a967745ccc65a901072
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-03-06 22:24:11 +0200

    create footer image component

---
+++ folio/src/components/Footer_image.js
@@ -0,0 +1,20 @@
+const FooterImage=({source})=>{
+
+
+    return (
+        <>
+        
+            <img src={source} style={{"height":"6rem"}} alt="Footer image"></img>
+     
+            
+            
+        </>
+    )
+
+
+}
+
+
+
+
+export default FooterImage;
