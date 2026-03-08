1d89b556ad7edaf8db14ef5837fc18f3e2df3cfd
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-03-08 17:06:29 +0200

    import images and adjust height and 100% width

--- folio/src/components/Work.js
+++ folio/src/components/Work.js
@@ -1,6 +1,15 @@
 import {useEffect} from 'react';
 import gsap from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import Hero from './Hero.js';
+import nagana from '../media/work_nagana.jpg';
+import poly from '../media/work_poly.jpg';
+import father_to_sons from '../media/work_father.jpg';
+import trip from '../media/work_trip.jpg';
+import work_self from '../media/work_selfie.jpg';
+import molao from '../media/work_molao.jpg';
+import dipaki from '../media/work_dipaki.jpg';
+
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -18,7 +27,7 @@ const tl=gsap.timeline();
                     trigger:"#spane-2",
                     start:"top 99.8%",
                     end:"top bottom",
-                    scrub:0.5,
+                    scrub:1,
 
                 }
             })
@@ -29,22 +38,81 @@ const tl=gsap.timeline();
                         <div className="container-fluid">
                                 <div className="row row-cols-1 row-cols-lg-1">
                                     
-                            <div className="col bg-success min-vh-100 border position-relative" id="work-intro">
-                                    <p>work intro</p>
-                                <div className="border bg-primary min-vh-100 position-absolute top-0 bottom-0 start-0 end-0" id="spane-1">
-                                    spane 1
+                            <div className="col min-vh-100  position-relative d-flex justify-content-center" id="work-intro">
+                                    <Hero section="works." heading="Works"/>
+                                <div className="bg-primary min-vh-100 position-absolute top-0 bottom-0 start-0 end-0" id="spane-1"></div>
+                            </div>
+
+                            {/*work 1*/}
+                            <div className="col d-lg-block d-md-block border " id="spane-2">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={poly} className="work-image"></img>
+                                </div>
+                                <div className="col  d-flex justify-content-center">
+                                    spane title
                                 </div>
                             </div>
 
-
-                            <div className="col min-vh-100 border" id="spane-2">
-                            </div> 
-                            <div className="col min-vh-100 border" id="spane-3">
+                            {/*work 2*/}
+                            <div className="col d-lg-block d-md-block " id="spane-3">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={nagana} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
                             </div>
-                            <div className="col min-vh-100 border" id="spane-4">
-                            </div>         
-                            <div className="col min-vh-100 border" id="spane-5">
-                            </div> 
+                            
+                            {/*work 3*/}
+                             <div className="col d-lg-block d-md-block " id="spane-3">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={father_to_sons} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
+                            </div>
+                            
+                            {/*work 4*/}
+                             <div className="col d-lg-block d-md-block  " id="spane-3">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={trip} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
+                            </div>
+
+                             {/*work 5*/}           
+                             <div className="col d-lg-block d-md-block " id="spane-3">
+                                <div className="col  d-flex justify-content-center">
+                                    <img src={work_self} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
+                            </div>
+
+                            {/*work 6*/}
+                             <div className="col d-lg-block d-md-block " id="spane-3">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={molao} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
+                            </div>
+
+                            {/*work 7*/}
+                            <div className="col d-lg-block d-md-block " id="spane-3">
+                                <div className="col d-flex justify-content-center">
+                                    <img src={dipaki} className="work-image"></img>
+                                </div>
+                                <div className="col d-flex justify-content-center">
+                                    spane title
+                                </div>
+                            </div>
+
                             </div>
                         </div>
                         
