96c7a24d43b9b3a364d20dbbde7ddc4417c44a50
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-03-24 16:11:01 +0200

    use react Links

--- folio/src/components/Navbar.js
+++ folio/src/components/Navbar.js
@@ -7,6 +7,7 @@ import exhibitions from '../media/website_exhibitions.png';
 import gsap from 'gsap';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 import logo_image from '../media/website_offcanvas_logo3.jpg';
+import { Link } from 'react-router-dom'
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -22,17 +23,17 @@ const Navbar= ()=>{
   <nav className=" navbar fixed-top " id="nav">
     
      
-          < a className="" href="/">
+          < Link className="" to="/">
             <img src={logo} className=" " id="logo" alt="main_logo.svg"/>
-          </a>
+          </Link>
            
   
       <div className="d-none d-lg-flex d-md-block me-md-2 pb-md-4  gap-5 me-lg-5" id="fullscreen-nav">
-          <a className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" href="/about" >About</a>
-          <a className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" href="/work"> Work </a>
-          <a className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" href="/process"> My process </a>
-          <a className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" href="/exhibitions">Exhibitions </a>          
-          <a className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" href="/contact">Contact </a>
+          <Link className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" to="/about" >About</Link>
+          <Link className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" to="/work"> Work </Link>
+          <Link className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" to="/process"> My process </Link>
+          <Link className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" to="/exhibitions">Exhibitions </Link>          
+          <Link className="link-underline link-underline-opacity-0 tomorrow-thin me-md-2" to="/contact">Contact </Link>
       </div>
       <div className="d-lg-none d-md-none">
         <button className="navbar-toggler  border-0 mb-3 me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -41,30 +42,30 @@ const Navbar= ()=>{
       </div>
 
     {/*off canvas navigation items*/}
-      <div className="offcanvas w-75 offcanvas-end rounded-0 border-0 " tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas w-75 offcanvas-end rounded-0 border-0 " tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div className="offcanvas-header px-5">
           <div className="offcanvas-title" id="offcanvasNavbarLabel">
-            <a href="/">
+            <Link to="/">
               <img src={logo_image} id="offcanvas-logo" alt="logo.svg"></img>
-            </a>
+            </Link>
           </div>
         </div >
           <hr/>
-         <a className="link-underline link-underline-opacity-0 mx-auto" href="/about" >
+         <Link className="link-underline link-underline-opacity-0 mx-auto " to="/about" >
           <img id="offset-about" src={about} alt="about.svg"/>
-         </a><hr />
-          <a className="link-underline p-md-5 link-underline-opacity-0 mx-auto" href="/work" >
+         </Link><hr />
+          <Link className="link-underline p-md-5 link-underline-opacity-0 mx-auto" to="/work" >
             <img id="offset-work"  src={work} alt="work.svg"/>
-          </a><hr/>
-          <a className="link-underline link-underline-opacity-0 mx-auto" href="/process" >
+          </Link><hr/>
+          <Link className="link-underline link-underline-opacity-0 mx-auto " to="/process" >
             <img id="offset-process" src={process} alt=" process.svg"/>
-          </a><hr/>
-           <a className="link-underline link-underline-opacity-0 mx-auto" href="/exhibitions" >
+          </Link><hr/>
+           <Link className="link-underline link-underline-opacity-0 mx-auto " to="/exhibitions" >
             <img id="offset-exhibitions" src={exhibitions} alt="exhibitions.svg"/>
-          </a><hr/>
-          <a className="link-underline link-underline-opacity-0 mx-auto" href="/contact" >
+          </Link><hr/>
+          <Link className="link-underline link-underline-opacity-0 mx-auto" to="/contact" >
             <img id="offset-contact" src={contact} alt="contact.svg"/>
-          </a>
+          </Link>
       
       </div>
   </nav>
