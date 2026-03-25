dac3658598dbf377d0eeeb2b59439b457777d0e2
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-03-25 14:02:40 +0200

    Add scroll to top components

--- folio/src/App.js
+++ folio/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.js'
+import {Switch} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import About from './components/About.js';
+import Process from './components/Process.js';
+import Work from './components/Work.js'
+import Contact from './components/Contact.js';
+import Exhibitions from './components/Exhibitions.js';
+import ScrollToTop from './components/ScrollToTop.js'
+
+
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className="App">
+          <ScrollToTop/>
+          <Switch>
+            <Route path="/" exact >
+              <Home/>
+            </Route>
+            <Route path="/about" exact>
+              <About/>
+            </Route>
+            <Route path="/work" exact>
+              <Work/>
+            </Route>
+            <Route path="/process" exact>
+              <Process/>
+            </Route>
+              <Route path="/exhibitions" exact>
+              <Exhibitions/>
+            </Route>
+            <Route path="/contact" exact>
+              <Contact/>
+            </Route>
+          </Switch>
+        </div>
+      </>
   );
 }
 
 export default App;
+
+
