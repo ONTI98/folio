6b060e1b0463256837e1430649b6c233e221fdc0
Ontisitse Manyeneng <ontimanyeneng440@gmail.com>
2026-03-27 21:57:52 +0200

    add styling to buttton

--- folio/src/components/ScrollUpButton.js
+++ folio/src/components/ScrollUpButton.js
@@ -6,8 +6,12 @@ const scrollup=()=>{
 const ScrollUpButton=({id})=>{
     return (
         <>
-        <div className="position-absolute end-0 bottom-0">
-            <button onClick={scrollup} className="btn scrollupbutton"  id={id}>Scroll to Top</button>
+        <div className="position-absolute  end-0 bottom-0 mx-5 d-flex justify-content center m-2" id="scrollupbutton-container" >
+            
+            <button onClick={scrollup} className="btn scrollupbutton shadow rounded-pill mb-2 "  id={id}>
+                Back to Top
+                <i className="fa fa-xl fa-arrow-up"></i></button>
+                
         </div>
         </>
     )
